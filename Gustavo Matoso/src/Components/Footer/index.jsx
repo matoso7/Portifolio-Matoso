2fb8d4e91619a48383446a79272974cc88d8a787
matoso <99209377+matoso7@users.noreply.github.com>
2023-07-13 09:33:35 -0300

    Add files via upload

---
+++ Gustavo Matoso/src/Components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito com ❣️ por Gustavo Matoso © 2023
+        </footer>
+    )
+}
+
+export default Footer
